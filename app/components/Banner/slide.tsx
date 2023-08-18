e0e17e0263df2eb5dae025d5d347a65483aeb3da
Igor Piliavskyi <ipiliavskyi@gmail.com>
2023-08-18 17:20:13 +0200

    #5 Slider module

---
+++ app/components/Banner/slide.tsx
@@ -0,0 +1,42 @@
+// slide.tsx
+import Image from "next/image";
+
+interface SlideProps {
+  buttonFirstText: string;
+  buttonSecondText: string;
+  title: string;
+  imageSrc: string;
+}
+
+const Slide = ({ buttonFirstText, buttonSecondText, title, imageSrc }: SlideProps) => {
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 my-16">
+        <div className="grid justify-items-center mx-auto">
+          <div className="py-3 text-center lg:text-start">
+            <button className="text-blue bg-lightblue hover:shadow-xl text-sm md:text-lg font-bold px-6 py-1 rounded-3xl tracking-wider hover:text-white hover:bg-black">
+              {buttonFirstText}
+            </button>
+          </div>
+          <div className="py-3 text-center lg:text-start">
+            <h1 className="text-7xl lg:text-70xl font-bold text-darkpurple">
+              {title}
+            </h1>
+          </div>
+          <div className="my-7 text-center lg:text-start">
+            <button className="text-sm md:text-xl font-semibold hover:shadow-xl bg-blue text-white py-3 px-6 md:py-5 md:px-14 rounded-full hover:bg-hoblue">
+              {buttonSecondText}
+            </button>
+          </div>
+        </div>
+        <div className="mx-auto">
+          <Image src={imageSrc} alt="hero-image" width={800} height={642} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Slide;
+
+
