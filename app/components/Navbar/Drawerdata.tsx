c8c7ba080fc13cc2c02d02e70cd210cccd681d29
Igor Piliavskyi <ipiliavskyi@gmail.com>
2023-12-08 17:28:01 +0100

    #121 Link to shop resolved

--- app/components/Navbar/Drawerdata.tsx
+++ app/components/Navbar/Drawerdata.tsx
@@ -29,31 +29,31 @@ const Data = () => {
 
     return (
         <>
-        <div className="rounded-md max-w-sm w-full mx-auto">
-            <div className="flex-1 space-y-4 py-1">
-                <div className="sm:block">
-                    <div className="space-y-1 px-5 pt-2 pb-3">
-                        {navigation.map((item) => (
-                            <Link
-                                key={item.name}
-                                href={item.href}
-                                className={classNames(
-                                    item.current ? 'bg-gray-900 text-purple' : 'text-black hover:bg-gray-700 hover:text-purple',
-                                    'block  py-2 rounded-md text-base font-medium'
-                                )}
-                                aria-current={item.current ? 'page' : undefined}
-                            >
-                                {item.name}
-                            </Link>
-                        ))}
+            <div className="rounded-md max-w-sm w-full mx-auto">
+                <div className="flex-1 space-y-4 py-1">
+                    <div className="sm:block">
+                        <div className="space-y-1 px-5 pt-2 pb-3">
+                            {navigation.map((item) => (
+                                <Link
+                                    key={item.name}
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-purple' : 'text-black hover:bg-gray-700 hover:text-purple',
+                                        'block  py-2 rounded-md text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Link>
+                            ))}
 
-                        <div className="mt-4"></div>
+                            <div className="mt-4"></div>
 
-                      <Contactusform/>
+                            <Contactusform />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
